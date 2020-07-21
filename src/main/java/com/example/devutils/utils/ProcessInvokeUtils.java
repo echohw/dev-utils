d4a9bf800daa8b6fa3d56b8a8bb138ebe3280d9e
echohw <echohwx@gmail.com>
2020-07-21 16:18:25 +0800

    Update ProcessInvokeUtils.java

--- src/main/java/com/example/devutils/utils/ProcessInvokeUtils.java
+++ src/main/java/com/example/devutils/utils/ProcessInvokeUtils.java
@@ -1,6 +1,9 @@
 package com.example.devutils.utils;
 
+import java.awt.Desktop;
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.function.Consumer;
 
 /**
@@ -8,19 +11,37 @@ import java.util.function.Consumer;
  */
 public class ProcessInvokeUtils {
 
+    private static final String CMD_SELECT_WINDOWS = "explorer /select,";
+
     public static Runtime getRuntime() {
         return Runtime.getRuntime();
     }
 
-    public static void invoke(String cmd, Consumer<Process> consumer) throws IOException, InterruptedException {
-        invoke(getRuntime(), cmd, consumer);
+    public static Desktop getDesktop() {
+        return Desktop.getDesktop();
     }
 
-    public static void invoke(Runtime runtime, String cmd, Consumer<Process> consumer) throws IOException, InterruptedException {
+    public static void invoke(String cmd, Consumer<Process> resConsumer) throws IOException, InterruptedException {
+        invoke(getRuntime(), cmd, resConsumer);
+    }
+
+    public static void invoke(Runtime runtime, String cmd, Consumer<Process> resConsumer) throws IOException, InterruptedException {
         Process process = runtime.exec(cmd);
-        consumer.accept(process);
+        if (resConsumer != null) resConsumer.accept(process);
         process.waitFor();
         process.destroy();
+    }
+
+    public static void open(File path) throws IOException {
+        getDesktop().open(path);
+    }
+
+    public static void browse(URI uri) throws IOException {
+        getDesktop().browse(uri);
+    }
+
+    public static void select(String path) throws IOException, InterruptedException {
+        invoke(CMD_SELECT_WINDOWS + path, null);
     }
 
 }
